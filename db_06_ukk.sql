d4e1a8ae742c21556febd746befea573c72bd1b7
wijayad6 <155601952+wijayad6@users.noreply.github.com>
2024-02-20 15:53:33 +0700

    Add files via upload

---
+++ db_06_ukk.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Feb 20, 2024 at 08:52 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.2.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_06_ukk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bukus`
+--
+
+CREATE TABLE `bukus` (
+  `buku_id` int UNSIGNED NOT NULL,
+  `judul` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `penulis` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `penerbit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tahun_terbit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bukus`
+--
+
+INSERT INTO `bukus` (`buku_id`, `judul`, `penulis`, `penerbit`, `tahun_terbit`, `created_at`, `updated_at`) VALUES
+(2, 'Buku MTK', 'Anas Chaeru', 'RPL vtr', '2006', '2024-02-19 19:36:33', '2024-02-19 19:36:33'),
+(3, 'Buku IPA', 'Anas Chaeru', 'RPL vtr', '2006', '2024-02-19 19:36:44', '2024-02-19 19:36:44'),
+(4, 'Buku IPS', 'Anas Chaeru', 'RPL vtr', '2006', '2024-02-19 19:37:00', '2024-02-19 19:37:00'),
+(5, 'Buku Tulis', 'Harist', 'RPL vtr', '2006', '2024-02-20 00:11:08', '2024-02-20 00:11:08'),
+(6, 'Buku Gambar', 'Harist', 'RPL vtr', '2006', '2024-02-20 01:06:16', '2024-02-20 01:06:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint UNSIGNED NOT NULL,
+  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_users_table', 1),
+(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
+(3, '2019_08_19_000000_create_failed_jobs_table', 1),
+(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(5, '2024_02_19_010959_create_roles_table', 1),
+(6, '2024_02_19_080452_create_bukus_table', 1),
+(7, '2024_02_19_091047_create_pinjams_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password_reset_tokens`
+--
+
+CREATE TABLE `password_reset_tokens` (
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tokenable_id` bigint UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `abilities` text COLLATE utf8mb4_unicode_ci,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pinjams`
+--
+
+CREATE TABLE `pinjams` (
+  `peminjaman_id` int UNSIGNED NOT NULL,
+  `buku_id` int NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint UNSIGNED NOT NULL,
+  `role_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`, `created_at`, `updated_at`) VALUES
+(1, 'admin', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(2, 'petugas', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(3, 'user', '2024-02-19 19:01:05', '2024-02-19 19:01:05');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role_id` bigint UNSIGNED NOT NULL,
+  `alamat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `name`, `username`, `email`, `email_verified_at`, `password`, `role_id`, `alamat`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Idell Harvey', 'rhills', 'gusikowski.kailyn@gmail.com', '2024-02-19 19:01:05', '$2y$12$61/3RUCz4dzhiM3wKMOOgu/fehAnBdbYBNFpjwiSxGzFyaqUtcxRi', 2, '43525 Kessler Roads Apt. 203\nNadertown, MI 05182-5746', '6EuQoTTSJLG6tesNzjb0Dl4G6yXd6hE1dgqJgapukvXJcdhWPTrhmLWBooRC', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(2, 'Alta Ruecker', 'conner.stracke', 'raegan89@fisher.biz', '2024-02-19 19:01:05', '$2y$12$61/3RUCz4dzhiM3wKMOOgu/fehAnBdbYBNFpjwiSxGzFyaqUtcxRi', 2, '634 Letha Village\nEduardoside, IN 35177', 'qSHmFtQamH', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(3, 'Cathryn Hammes', 'antonina.ferry', 'oconner.nia@gmail.com', '2024-02-19 19:01:05', '$2y$12$61/3RUCz4dzhiM3wKMOOgu/fehAnBdbYBNFpjwiSxGzFyaqUtcxRi', 3, '714 Mante Plaza Suite 439\nEast Olaf, WI 05590-9368', 'OjYpGtsUjw2zSvBKkA7f5MSqnItcxFiAqspRfvcPt2shglYD24Ocmda4RRBV', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(4, 'Ray Walker', 'tlittel', 'salma24@yahoo.com', '2024-02-19 19:01:05', '$2y$12$61/3RUCz4dzhiM3wKMOOgu/fehAnBdbYBNFpjwiSxGzFyaqUtcxRi', 3, '1847 Herman Lakes\nEast Raina, TX 65160-3640', 'zJ3LWEJxe5126UYAX3VozPLWdM1tSwJ3s2G01b28rnMaY0ddZajrZezPa8je', '2024-02-19 19:01:05', '2024-02-19 19:01:05'),
+(5, 'Delores Schmeler', 'elsa.schumm', 'utreutel@gmail.com', '2024-02-19 19:01:05', '$2y$12$61/3RUCz4dzhiM3wKMOOgu/fehAnBdbYBNFpjwiSxGzFyaqUtcxRi', 1, '23497 Nellie Port\nSimonisfort, SD 21651', 'OqlvRbQjvnWJUmWESlxdlKvOR8vsu60LiApWcThM8eIUGwIZPctcyyOKyHwu', '2024-02-19 19:01:05', '2024-02-19 19:01:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bukus`
+--
+ALTER TABLE `bukus`
+  ADD PRIMARY KEY (`buku_id`);
+
+--
+-- Indexes for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `password_reset_tokens`
+--
+ALTER TABLE `password_reset_tokens`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Indexes for table `pinjams`
+--
+ALTER TABLE `pinjams`
+  ADD PRIMARY KEY (`peminjaman_id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `roles_role_name_unique` (`role_name`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bukus`
+--
+ALTER TABLE `bukus`
+  MODIFY `buku_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pinjams`
+--
+ALTER TABLE `pinjams`
+  MODIFY `peminjaman_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
